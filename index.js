1157f7dc555aaf8fe99b491c295091aa2bf4538b
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-07-12 17:29:14 +0800

    feat: auto deploy

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const koa = require('koa2');
 const crawl = require('./crawler/crawl_ssr4');
 const string2base64 = require('./utils/string2base64');
 const ss2ssr = require('./utils/ss2ssr');
+const koaRouter = require('koa-router');
+const exec = require('child_process').exec;
 
-const app = new koa();
-
-app.use(async ctx => {
+const router = new koaRouter();
+router.get('/',async ctx => {
   let data = await crawl();
   ctx.body = string2base64(data.join("\n"));
-} )
+});
+router.post('/deploy',async ctx => {
+  const result = await new Promise((resolve,reject) => {
+    exec('./deploy.sh',function(err,res){
+      if(err){
+        reject(err);
+      }
+      else{
+        resolve(res);
+      }
+    })
+  })
+  ctx.body=result;
+})
+const app = new koa();
+app.use(router.routes());
 app.listen(3001);
