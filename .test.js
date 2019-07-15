2b5f2d5cb8b142f813f9409ae788980d966be994
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-07-16 01:17:45 +0800

    feat: new crawl

--- .test.js
+++ .test.js
@@ -1,2 +1,31 @@
-const craw = require('./crawler/crawl_ssr9');
-craw().then(res => console.log(res));
+// const hosts=[
+//   {
+//     ip:'23.239.20.120',
+//     // port:8097
+//     port:8888
+//   },
+//   {
+//     ip:'54.92.86.43',
+//     port:14139
+//   },
+//   {
+//     ip:'188.119.64.20',
+//     port:48887
+//   }
+// ]
+
+
+
+// const probe = require('./utils/probe');
+// const crawler = require('./crawler/index');
+// const tcpp = require('tcp-ping');
+
+// hosts.forEach(async item => {
+//   const ojbk = await probe(item);
+//   console.log(ojbk);
+// })
+// // probe(hosts[0]).then(res => console.log(res))
+
+// // tcpp.ping({})
+
+require('./crawler/crawl_ssr10')().then(res => console.log(res))
--- crawler/index.js
+++ crawler/index.js
@@ -5,9 +5,10 @@ const crawl4 = require('./crawl_ssr7');
 const crawl5 = require('./crawl_ssr8');
 const crawl6 = require('./crawl_ssr2');
 const crawl7 = require('./crawl_ssr');
+const crawl8 = require('./crawl_ssr10');
 
 const crawler = async () => {
-  const response = await Promise.all([crawl1(),crawl2(),crawl3(),crawl4(),crawl5(),crawl6(),crawl7()]);
+  const response = await Promise.all([crawl1(),crawl2(),crawl3(),crawl4(),crawl5(),crawl6(),crawl7(),crawl8()]);
   const data = response.reduce((sum,item) => sum.concat(item) ,[]);
   return data;
 }
