2671e168eb41cd28063fd3869dd502e9161a2e6f
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-07-21 12:21:28 +0800

    feat: add new crawl

--- crawler/index.js
+++ crawler/index.js
@@ -9,9 +9,10 @@ const crawl8 = require('./crawl_ssr10');
 const crawl9 = require('./crawl_ssr11');
 const crawl10 = require('./crawl_ssr13');
 const crawl11 = require('./crawl_ssr14');
+const crawl12 = require('./crawl_ssr15');
 
 const crawler = async () => {
-  const response = await Promise.all([crawl1(),crawl2(),crawl3(),crawl4(),crawl5(),crawl6(),crawl7(),crawl8(),crawl9(),crawl10(),crawl11()]);
+  const response = await Promise.all([crawl1(),crawl2(),crawl3(),crawl4(),crawl5(),crawl6(),crawl7(),crawl8(),crawl9(),crawl10(),crawl11(),crawl12()]);
   const data = response.reduce((sum,item) => sum.concat(item) ,[]);
   return data;
 }
